4a8fc0a08990668f9c5141eeaf98292262724553
KevKsar <cassart.kev@gmail.com>
2021-03-01 16:03:48 +0100

    up

--- dart-sass/commande.bat
+++
@@ -1,6 +0,0 @@
-sass sass/styleEmail.scss email-validation/styleEmail.css --watch
-sass sass/reserveRoom.scss reserve-room/reserveRoom.css --watch
-sass sass/indieClub.scss indie-club/indieClub.css --watch
-sass sass/places.scss places/places.css --watch
-sass sass/subPlan.scss sub-plan/subPlan.css --watch
-sass sass/corpWebsite.scss corpWebsite/corpWebsite.css --watch
